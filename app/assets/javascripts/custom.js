a06e4ac2bd14d469e1cbff7db5408fe83ad79d84
Thanh Huynh <thanhjamin@yahoo.com>
2014-01-26 21:58:53 -0800

    reduce bootstrap assets

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -31,3 +31,34 @@ $(function(){
         });
   });
 
+
+(function($) {
+    jQuery(document).ready(function($) {
+        $(".scroll").click(function(event){ // When a link with the .scroll class is clicked
+            event.preventDefault(); // Prevent the default action from occurring
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500); // Animate the scroll to this link's href value
+        });
+    });
+})(jQuery);
+
+$(document).ready(function() {
+
+var cnt=0, texts=[];
+
+// save the texts in an array for re-use
+$(".textContent").each(function() {
+  texts[cnt++]=$(this).text();
+});
+function slide() {
+  if (cnt>=texts.length) cnt=0;
+  $('#textMessage').html(texts[cnt++]);
+  $('#textMessage')
+    .fadeIn('slow').animate({opacity: 1.0}, 2000).fadeOut('slow',
+     function() {
+       return slide()
+     }
+  );
+}
+slide()
+
+});
